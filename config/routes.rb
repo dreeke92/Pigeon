43ab3e89acda97a36c931f72aff5c5945d6ec430
Andreas Van Assche <andreasava@hotmail.com>
2019-01-17 12:53:49 +0100

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'posts/donations'
-  get 'posts/follows'
-  get 'posts/ratings'
-  get 'posts/likes'
-  get 'posts/comments'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :charities
+  resources :comments, only: [ :create, :update, :destroy]
+  resources :donations, only: [ :create]
+  resources :follows, only: [ :create, :update]
+  resources :likes, only: [ :create, :update]
+  resources :posts, only: [ :new, :create, :edit, :update]
+  resources :ratings, only: [ :create, :update]
+
 end
