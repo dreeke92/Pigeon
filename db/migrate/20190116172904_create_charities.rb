07ee218cb084fa1e4304f4cfea282c2834bbfa79
Andreas Van Assche <andreasava@hotmail.com>
2019-01-21 17:35:42 +0100

    progress

--- db/migrate/20190116172904_create_charities.rb
+++ db/migrate/20190116172904_create_charities.rb
@@ -3,7 +3,7 @@ class CreateCharities < ActiveRecord::Migration[5.2]
     create_table :charities do |t|
       t.string :name
       t.text :description
-      t.string :profile_picture
+      t.string :photo
       t.string :website_url
       t.integer :longitude
       t.integer :latitude
