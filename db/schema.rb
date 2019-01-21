93ad752d184558077e9ae58849dd511e3d32a1ad
Andreas Van Assche <andreasava@hotmail.com>
2019-01-21 16:13:20 +0100

    first charity show and index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_16_173607) do
+ActiveRecord::Schema.define(version: 2019_01_21_111438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 2019_01_16_173607) do
     t.integer "latitude"
     t.string "phone_number"
     t.integer "total_credit"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
