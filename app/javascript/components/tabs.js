972361865a51262d9971f5de1429be54166ef455
Andreas Van Assche <andreasava@hotmail.com>
2019-01-31 14:10:53 +0100

    fixed

--- app/javascript/components/tabs.js
+++ app/javascript/components/tabs.js
@@ -35,31 +35,15 @@ function showCurrentPage(event){
   };
 };
 
-allTabs.forEach((charityTab) => {
-  charityTab.addEventListener('click', (event)=>{
-    removeActiveTabs();
-    activateCurrentTab(event);
-    hideAllPages();
-    showCurrentPage(event);
+function initTabs(){
+  allTabs.forEach((charityTab) => {
+    charityTab.addEventListener('click', (event)=>{
+      removeActiveTabs();
+      activateCurrentTab(event);
+      hideAllPages();
+      showCurrentPage(event);
+    });
   });
-});
+};
 
-var elem = document.querySelector('.grid');
-var msnry = new Masonry( elem, {
-  // options
-  itemSelector: '.grid-item',
-  columnWidth: 200
-});
-
-// element argument can be a selector string
-//   for an individual element
-var msnry = new Masonry( '.grid', {
-  // options
-});
-
-$('.grid').masonry({
-  itemSelector: '.grid-item',
-  columnWidth: 160
-});
-
-
+export { initTabs };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,4 @@
 import "bootstrap";
 // import "/path/to/masonry.pkgd.min.js"
+import { initComponents } from '../components/init';
+initComponents ();
