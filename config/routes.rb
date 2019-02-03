38b6aaa2ddee62c538435cde4dea0c7588539de1
Andreas Van Assche <andreasava@hotmail.com>
2019-02-03 10:20:22 +0100

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :controllers
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :charities
   resources :comments, only: [ :create, :update, :destroy]
   resources :donations, only: [ :create]
