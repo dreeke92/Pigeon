e12b296768f9e9d7f15b2f689f3a02cf796cf695
Andreas Van Assche <andreasava@hotmail.com>
2019-02-11 21:07:46 +0100

    first steps to post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,22 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new()
+    @charity = Charity.find(params)
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @charity = Charity.find(params[:charity_id])
+    @post.charity = @charity
+    @post.save
+    redirect_to charity_path(@charity)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
